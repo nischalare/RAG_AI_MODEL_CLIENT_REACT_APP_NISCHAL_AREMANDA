a8337aa3e30a020dc7efaafd5cb638060bd8b476
Nischal Aremanda <nischal.aremanda1261994@gmail.com>
2026-02-26 21:07:27 +0530

    Update authService.js

--- src/api/authService.js
+++ src/api/authService.js
@@ -1,11 +1,35 @@
 import api from "./axiosInstance";
 
-export const loginUser = async (data) => {
-  const response = await api.post("/login", data);
+/**
+ * Login user using OAuth2 form format
+ * FastAPI expects:
+ *  - username
+ *  - password
+ *  - application/x-www-form-urlencoded
+ */
+export const loginUser = async (email, password) => {
+  const formData = new URLSearchParams();
+  formData.append("username", email);   // OAuth2 expects username
+  formData.append("password", password);
+
+  const response = await api.post(
+    "/auth/login",
+    formData,
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    }
+  );
+
   return response.data;
 };
 
+
+/**
+ * Register user (JSON format)
+ */
 export const registerUser = async (data) => {
-  const response = await api.post("/register", data);
+  const response = await api.post("/auth/register", data);
   return response.data;
 };
